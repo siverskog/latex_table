0495c80d8b2907429ea38f12300fb4e0b671d234
siverskog <jonathan.siverskog@gmail.com>
2015-08-18 18:51:44 +0200

    Update latex_table.R

--- latex_table.R
+++ latex_table.R
@@ -112,7 +112,7 @@ header <- function(h, cmid = TRUE) {
 
 latex.table <- function(h, t, size, type = "tabularx", align = c("l", rep("X", ncol))) {
   
-  begin <- .begin.table(size, type)
+  begin <- .begin.table(size, type, align)
   end <- .end.table(type)
   
   tab <- NULL
